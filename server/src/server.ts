51ab961e7c9caca396af35f2140019bd988d638d
Rony de Lucca <ronylucca@gmail.com>
2020-06-22 01:59:20 -0300

    routes included

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,37 @@
-import express from 'express'
+import express, { response } from 'express'
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de usuarios')
-    response.json([
+
+const users = [
         'Rony',
         'Raquel', 
         'Bella',
         'Charlotte',
         'Tiffany'
-    ])
+    ]
+
+
+app.get('/users', (request, response) => {
+    console.log('Listagem de usuarios')
+    response.json(users)
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const user = users[id];
+
+    return response.json(user)
 })
+
+app.post('/users', (request, response) => {
+    const user = {
+        name: "Rony",
+        email: "ronylucca@gmail.com"
+    }
+
+    return response.json(user);
+})
+
 
 app.listen(3000)
