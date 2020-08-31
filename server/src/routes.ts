cc2d9282cd673deeb1663bbb78b5f3453e12c450
Rony de Lucca <ronylucca@gmail.com>
2020-08-30 22:54:32 -0300

    route updated

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,7 +2,7 @@ import express, { Router, request, response } from 'express';
 import knex from './database/connection';
 
 const routes = express.Router();
-
+            
 
 
 //Items
@@ -10,13 +10,12 @@ const routes = express.Router();
 routes.get('/items', async (request, response) => {
 
     const items = await knex('items').select('*');
-
-    const serializedItems = items.map(item => {
+    const serializedItems = items.map(item => {       
         return {
             id: item.id,
             title: item.title,
             image_url: `http://localhost:3333/uploads/${item.image}`,
-        };
+        };            
     });
     return response.json(serializedItems);
 });
@@ -46,9 +45,9 @@ routes.post('/points', async(request, response) => {
         uf
 
     });
-
+    
+    
     const point_id = insertedIds[0];
-
     const pointItems = items.map((item_id: number) => {
 
         return {
